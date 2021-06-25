54b0941f8cc5e827191d641944daacf46fc5d207
Shantanu34 <shantanu@viga.studio>
2021-06-25 18:35:58 +0530

    Add project files.

---
+++ ArcadeApp2/main.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <SDL/SDL.h>
+
+#include "Graphics/Color.h"
+#include "Graphics/Screen.h"
+#include "Shapes/Line2D.h"
+
+static const int SCREEN_WIDTH = 224;
+static const int SCREEN_HEIGHT = 288;
+static const int MAGNIFICATION = 2;
+
+int main(int argc, char** argv)
+{
+	Screen theScreen;
+
+	theScreen.Init(SCREEN_WIDTH, SCREEN_HEIGHT, MAGNIFICATION);
+
+	Line2D line = { Vec2D(0, 0), Vec2D(SCREEN_WIDTH, SCREEN_HEIGHT) };
+
+	theScreen.Draw(line, Color::White());	
+	//theScreen.Draw(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, Color::Yellow());
+	theScreen.SwapScreens();
+
+	SDL_Event sdlEvent;
+	bool running = true;
+
+
+	while(running)
+	{
+		while(SDL_PollEvent(&sdlEvent))
+		{
+			switch (sdlEvent.type)
+			{
+			case SDL_QUIT:
+				running = false;
+				break;
+			}
+		}
+	}
+
+
+	return EXIT_SUCCESS;
+}
+
